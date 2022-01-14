2fd0415a05da7b50fb1bf9127aa0385fb05d5562
Aitzhan <aitzhan@mail.ru>
2022-01-14 19:03:26 +0600

    feat: add the-house-picked block and pick random element functionality

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -17,18 +17,26 @@ type GameProps = {
 };
 
 export const Game = ({ handleFinishGame }: GameProps) => {
-  const [started, setStarted] = useState(false);
+  // const [started, setStarted] = useState(false);
   const [selected, setSelected] = useState<string | null>(null);
+  const [housePicked, setHousePicked] = useState<string | null>(null);
+
+  const allVars = Object.values(VARS);
 
   const handleStartGame = (selectedVar: string) => {
-    setStarted(true);
+    // setStarted(true);
     setSelected(selectedVar);
+    const triangleBg = document.querySelector('.game-your-choice')!;
+    triangleBg.classList.add('game-your-choice--selected');
+    const item = allVars[Math.floor(Math.random() * allVars.length)];
+    setHousePicked(item);
   };
 
   const handlePlayAgain = (): void => {
-    console.log('click');
-    setStarted(false);
+    // setStarted(false);
     setSelected(null);
+    const triangleBg = document.querySelector('.game-your-choice')!;
+    triangleBg.classList.remove('game-your-choice--selected');
   };
 
   return (
@@ -37,21 +45,43 @@ export const Game = ({ handleFinishGame }: GameProps) => {
         Play again
       </button>
       <div className="game">
-        <Variant
-          name={VARS.PAPER}
-          handleClick={handleStartGame}
-          isVisible={selected === null || selected === VARS.PAPER}
-        />
-        <Variant
-          name={VARS.SCISSORS}
-          handleClick={handleStartGame}
-          isVisible={selected === null || selected === VARS.SCISSORS}
-        />
-        <Variant
-          name={VARS.ROCK}
-          handleClick={handleStartGame}
-          isVisible={selected === null || selected === VARS.ROCK}
-        />
+        {selected && (
+          <div className="game-subtitle">
+            <p>Your choice</p>
+            <p>The house picked</p>
+          </div>
+        )}
+        <div className="game-field">
+          <div className="game-your-choice">
+            <Variant
+              name={VARS.PAPER}
+              handleClick={handleStartGame}
+              isVisible={selected === null || selected === VARS.PAPER}
+              isSelected={selected === VARS.PAPER}
+            />
+            <Variant
+              name={VARS.SCISSORS}
+              handleClick={handleStartGame}
+              isVisible={selected === null || selected === VARS.SCISSORS}
+              isSelected={selected === VARS.SCISSORS}
+            />
+            <Variant
+              name={VARS.ROCK}
+              handleClick={handleStartGame}
+              isVisible={selected === null || selected === VARS.ROCK}
+              isSelected={selected === VARS.ROCK}
+            />
+          </div>
+          <div className="game-house-picked">
+            <Variant
+              name={housePicked || ''}
+              handleClick={handleStartGame}
+              isVisible={selected !== null}
+              isSelected={false}
+              isInHouse={true}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/Game/Variant.tsx
+++ src/components/Game/Variant.tsx
@@ -6,16 +6,29 @@ type VariantProps = {
   name: string;
   handleClick: (n: string) => void;
   isVisible: boolean;
+  isSelected: boolean;
+  isInHouse?: boolean;
 };
 
-export const Variant = ({ name, handleClick, isVisible }: VariantProps) => {
+export const Variant = ({
+  name,
+  handleClick,
+  isVisible,
+  isSelected,
+  isInHouse,
+}: VariantProps) => {
+  let classes;
+  if (isInHouse) {
+    classes = `variant  variant--${name}  variant--house-picked`;
+  } else {
+    classes = isSelected
+      ? `variant  variant--${name}  variant--selected`
+      : `variant  variant--${name}`;
+  }
   return (
     <>
       {isVisible && (
-        <div
-          className={`variant  variant--${name}`}
-          onClick={() => handleClick(name)}
-        ></div>
+        <div className={classes} onClick={() => handleClick(name)}></div>
       )}
     </>
   );
